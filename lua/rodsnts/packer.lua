d57c2ef9366323cbdc670e851a502d0c317e7e19
Rodrigo Santos <rr.santos1232@gmail.com>
2023-07-29 01:06:03 +0100

    ADD: formatter

--- lua/rodsnts/packer.lua
+++ lua/rodsnts/packer.lua
@@ -4,10 +4,16 @@
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
-	-- Packer can manage itself
-	use 'wbthomason/packer.nvim'
+  -- Packer can manage itself
+  use 'wbthomason/packer.nvim'
 
-  use {'folke/tokyonight.nvim'}
+  use { 'folke/tokyonight.nvim' }
+
+  use { 'neovim/nvim-lspconfig' }
+
+  use { 'jose-elias-alvarez/null-ls.nvim' }
+
+  use { 'MunifTanjim/prettier.nvim' }
 
   use({
     "utilyre/barbecue.nvim",
@@ -16,49 +22,49 @@ return require('packer').startup(function(use)
       "SmiteshP/nvim-navic",
       "nvim-tree/nvim-web-devicons", -- optional dependency
     },
-    after = "nvim-web-devicons", -- keep this if you're using NvChad
+    after = "nvim-web-devicons",     -- keep this if you're using NvChad
     config = function()
       require("barbecue").setup()
     end,
-  }) 
+  })
 
-	use {
-		'nvim-telescope/telescope.nvim', tag = '0.1.2',
-		-- or                            , branch = '0.1.x',
-		requires = { {'nvim-lua/plenary.nvim'} }
-	}
+  use {
+    'nvim-telescope/telescope.nvim', tag = '0.1.2',
+    -- or                            , branch = '0.1.x',
+    requires = { { 'nvim-lua/plenary.nvim' } }
+  }
 
   use { "LinArcX/telescope-command-palette.nvim" }
 
-	use {'nyoom-engineering/oxocarbon.nvim'}
+  use { 'nyoom-engineering/oxocarbon.nvim' }
 
-  use {'rodsnts/component_generator'}
+  use { 'rodsnts/component_generator' }
 
-  use {'rcarriga/nvim-notify'}
+  use { 'rcarriga/nvim-notify' }
 
-	use('nvim-treesitter/nvim-treesitter', {run = ':TSUpadte'})
+  use('nvim-treesitter/nvim-treesitter', { run = ':TSUpadte' })
 
-	use('nvim-treesitter/playground')
+  use('nvim-treesitter/playground')
 
   use('norcalli/nvim-colorizer.lua')
 
-	use('theprimeagen/harpoon')
+  use('theprimeagen/harpoon')
 
-  use {'hrsh7th/cmp-buffer'}
+  use { 'hrsh7th/cmp-buffer' }
 
-  use {'hrsh7th/cmp-path'}
+  use { 'hrsh7th/cmp-path' }
 
-	use('mbbill/undotree')
+  use('mbbill/undotree')
 
   use {
     'nvim-lualine/lualine.nvim',
     requires = { 'nvim-tree/nvim-web-devicons', opt = true }
   }
 
-	use('tpope/vim-fugitive')
+  use('tpope/vim-fugitive')
 
   use 'nvim-tree/nvim-web-devicons' -- OPTIONAL: for file icons
-  use 'lewis6991/gitsigns.nvim' -- OPTIONAL: for git status
+  use 'lewis6991/gitsigns.nvim'     -- OPTIONAL: for git status
   use 'romgrk/barbar.nvim'
 
   use {
@@ -68,24 +74,24 @@ return require('packer').startup(function(use)
     },
   }
 
-	use {
-		'VonHeikemen/lsp-zero.nvim',
-		branch = 'v2.x',
-		requires = {
-			-- LSP Support
-			{'neovim/nvim-lspconfig'},             -- Required
-			{                                      -- Optional
-			'williamboman/mason.nvim',
-			run = function()
-				pcall(vim.cmd, 'MasonUpdate')
-			end,
-		},
-		{'williamboman/mason-lspconfig.nvim'}, -- Optional
+  use {
+    'VonHeikemen/lsp-zero.nvim',
+    branch = 'v2.x',
+    requires = {
+      -- LSP Support
+      { 'neovim/nvim-lspconfig' }, -- Required
+      {                            -- Optional
+        'williamboman/mason.nvim',
+        run = function()
+          pcall(vim.cmd, 'MasonUpdate')
+        end,
+      },
+      { 'williamboman/mason-lspconfig.nvim' }, -- Optional
 
-		-- Autocompletion
-		{'hrsh7th/nvim-cmp'},     -- Required
-		{'hrsh7th/cmp-nvim-lsp'}, -- Required
-		{'L3MON4D3/LuaSnip'},     -- Required
-	}
-}
+      -- Autocompletion
+      { 'hrsh7th/nvim-cmp' },     -- Required
+      { 'hrsh7th/cmp-nvim-lsp' }, -- Required
+      { 'L3MON4D3/LuaSnip' },     -- Required
+    }
+  }
 end)
